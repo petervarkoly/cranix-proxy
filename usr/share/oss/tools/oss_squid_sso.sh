df6c484beb305f46d5e06e8722432b03acc8e595
Peter Varkoly <varkoly@suse.com>
2018-01-08 22:43:36 +0100

    New file oss_squid_sso.sh from oss-base.

---
+++ usr/share/oss/tools/oss_squid_sso.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TOKEN=$( grep de.openschoolserver.api.auth.localhost= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.api.auth.localhost=//' )
+
+while true
+do
+        read IP
+        user=$( curl -sX GET --header 'Content-Type: application/json' --header 'Accept: text/plain' $DATA --header 'Authorization: Bearer '${TOKEN} "http://localhost:9080/api/devices/loggedIn/$IP" )
+	if [ "$user" ];
+	then
+		echo "OK user=\"$user\""
+	else
+		echo "ERR user=\"No user logged in\""
+	fi
+done
